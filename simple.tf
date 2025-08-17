faf61e752fb17f34440977273abca1ee166cb7f6
neozhang <neozhang9233@gmail.com>
2025-08-17 11:38:41 +0800

    Initial commit: Add Terraform configuration and .gitignore

---
+++ simple.tf
@@ -0,0 +1,21 @@
+# main.tf
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# 通过环境变量或 aws configure 的默认配置读取凭证
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
